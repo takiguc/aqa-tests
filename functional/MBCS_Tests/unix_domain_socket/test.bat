33fde43c9c1ef326f3427711e3c7f6bba09087ba
KKES335 <kkes335@jp.ibm.com>
2021-11-16 18:31:51 +0900

    Add test.bat

---
+++ functional/MBCS_Tests/unix_domain_socket/test.bat
@@ -0,0 +1,29 @@
+@echo off
+rem Licensed under the Apache License, Version 2.0 (the "License");
+rem you may not use this file except in compliance with the License.
+rem You may obtain a copy of the License at
+rem
+rem      https://www.apache.org/licenses/LICENSE-2.0
+rem
+rem Unless required by applicable law or agreed to in writing, software
+rem distributed under the License is distributed on an "AS IS" BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing permissions and
+rem limitations under the License.
+
+SETLOCAL
+SET PWD=%~dp0
+call %PWD%\check_env_windows.bat
+
+SET OUTPUT=output.txt
+SET CP=%PWD%\unix_domain_socket.jar
+call %PWD%\..\data\setup_%LOCALE%.bat
+
+%JAVA_BIN%\java -cp %CP% Server %TEST_STRING% &
+timeout /nobreak 3 > /nul
+
+%JAVA_BIN%\java -cp %CP% Client %TEST_STRING% "%TEST_STRINGS%" &
+timeout /nobreak 3 > /nul
+
+fc output expected_result > fc.out 2>&1
+exit %errorlevel%
